04cdbcd919ce1dd53b348d94c3dcc4f689d5aa59
Dan Elkouby <streetwalrus@codewalr.us>
2016-12-03 21:04:06 +0200

    Nuke zram into oblivion
    Who the hell needs that on a phone with FOUR FUCKING GIGABYTES OF
    PHYSICAL MEMORY???

--- device.mk
+++ device.mk
@@ -211,7 +211,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.msm8994
 
-# Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.oneplus2.power.sh \
@@ -222,7 +221,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.oneplus2.rc \
-    init.zram.sh \
     ueventd.qcom.rc
 
 # RIL
