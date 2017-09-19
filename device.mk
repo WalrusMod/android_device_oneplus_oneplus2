68dad32a1fe03c17e475266bd0b7106d4b2e782c
thecrazyskull <anaskarbila@aospa.co>
2017-09-19 10:01:02 +0530

    op2: deprecate CameraParametersExtra.h
    Change-Id: Ice3bc6cabd970c6ad435f4ca7543e9102557ab9c

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    libshim_atomic 
+    libshim_atomic \
+    libop2_cam
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
